4f8db0abe10bdb24c4260e769bd4ca3fce86413c
Jojoooo1 <chevalier.jonathan@live.fr>
2020-05-12 09:36:07 -0300

    delete key from log

---
+++ Engineering/Dev/terraform/delete_project.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+if [[ ! -x "$(command -v gcloud)" ]]; then
+  echo "gcloud not found"
+  exit 1
+fi
+
+ENV="dev"
+PROJECT_NAME_START="$ENV-tf-project"
+#
+PROJECT_DETAILS=$(gcloud projects list | grep $PROJECT_NAME_START)
+#
+PROJECT_ID=$(echo $PROJECT_DETAILS | awk '{print $1}')
+#
+read -r -p "Are you sure you want to delete the project \"$PROJECT_ID\" [Y/n]:  " RESPONSE
+echo
+if [[ $RESPONSE =~ ^([yY][eE][sS]|[yY])$ ]]; then
+
+  gcloud projects delete $PROJECT_ID
+  rm -f keys/*
+
+else
+
+  echo
+  echo "Action cancelled exiting..."
+  exit 0
+
+fi
---
+++ Engineering/Dev/terraform/get_project_details.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [[ ! -x "$(command -v gcloud)" ]]; then
+  echo "gcloud not found"
+  exit 1
+fi
+
+ENV="dev"
+PROJECT_NAME_START="$ENV-tf-project"
+#
+PROJECT_DETAILS=$(gcloud projects list | grep $PROJECT_NAME_START)
+#
+PROJECT_ID=$(echo $PROJECT_DETAILS | awk '{print $1}')
+PROJECT_NAME=$(echo $PROJECT_DETAILS | awk '{print $2}')
+PROJECT_NUMBER=$(echo $PROJECT_DETAILS | awk '{print $3}')
+PROJECT_RANDOM_ID=${PROJECT_ID##*project-}
+#
+PROJECT_SERVICE_ACCOUNT_NAME="$ENV-tf-sa-$PROJECT_RANDOM_ID"
+BUCKET_NAME="$PROJECT_NAME-backend"
+
+echo "PROJECT_ID: $PROJECT_ID"
+echo "PROJECT_NAME: $PROJECT_NAME"
+echo "PROJECT_NUMBER: $PROJECT_NUMBER"
+echo "PROJECT_SERVICE_ACCOUNT_NAME: $PROJECT_SERVICE_ACCOUNT_NAME"
+echo "BUCKET_NAME: $BUCKET_NAME"
